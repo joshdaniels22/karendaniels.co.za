b821152acaad0a9ef5009ddcd873344719bd9296
Joshua Daniels <joshuadaniels@Joshuas-MacBook-Air.local>
2024-07-20 18:23:17 +0200

    replace content with maintenance notification

--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,49 @@
-<script lang="ts">
-  import backdrop from "./assets/3E49BBCB-09F0-468A-B59C-D9396BBB066D_1_201_a.jpeg";
-</script>
-
 <main>
-  <div id="all-content">
-    <img src={backdrop} alt="kayaking with dolphins" />
-    <h1>Karen Daniels</h1>
-    <!-- <hr style="width:100%;" /> -->
-    <h3>
-      Health Policy and Systems Researcher. <br />
-      Wandering and wondering human. <br />
-      Eternally committed to social justice. <br />
-      Unapologetically in service of the Feminine Divine.
-    </h3>
-    <ul style="list-style-type: none; ">
-      <li style="display: inline-block;">
-        <a href="https://twitter.com/KarensComments">twitter</a>
-      </li>
-      <li style="display: inline-block;">
-        <a href="https://www.linkedin.com/in/karen-daniels-1b4866123/"
-          >linkedin</a
-        >
-      </li>
-    </ul>
-    <h1>.</h1>
-    <!-- <hr style="width:100%; color: blue; " /> -->
-    <div id="sections">
-      <div id="left-column">
-        <div id="card">
-          <h3>Work</h3>
-          <ul>
-            <li>MRC</li>
-            <li>WHO</li>
-          </ul>
+    <div id="all-content">
+        <h1>Karen Daniels</h1>
+        <h3>
+            Health Policy and Systems Researcher. <br/>
+            Wandering and Wondering Human. <br/>
+            Eternally Committed to Social Justice. <br/>
+            Unapologetically in Service of The Feminine Divine.
+        </h3>
+        <h1></h1>
+        <h3>Website In Progress... Big Things Coming!!!</h3>
+        <!--
+        <div id="sections">
+            <div id="left-column">
+                <div id="card">
+                    <h3>Work</h3>
+                    <ul>
+                        <li>MRC</li>
+                        <li>WHO</li>
+                    </ul>
+                </div>
+                <div id="card">
+                    <h3>Publications</h3>
+                    <ul>
+                        <li>Nature</li>
+                        <li>Science</li>
+                    </ul>
+                </div>
+                <div id="card">
+                    <h3>Hobbies</h3>
+                    <ul>
+                        <li>Swimming</li>
+                        <li>Moming</li>
+                    </ul>
+                </div>
+            </div>
+            <div id="right-column">
+                <div id="card">
+                    <h3>Articles</h3>
+                    <ul>
+                        <li>Palestine</li>
+                        <li>Nursing</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div id="card">
-          <h3>Publications</h3>
-          <ul>
-            <li>Nature</li>
-            <li>Science</li>
-          </ul>
-        </div>
-        <div id="card">
-          <h3>Hobbies</h3>
-          <ul>
-            <li>Swimming</li>
-            <li>Moming</li>
-          </ul>
-        </div>
-      </div>
-      <div id="right-column">
-        <div id="card">
-          <h3>Articles</h3>
-          <ul>
-            <li>Palestine</li>
-            <li>Nursing</li>
-          </ul>
-        </div>
-      </div>
+        -->
     </div>
-  </div>
 </main>
